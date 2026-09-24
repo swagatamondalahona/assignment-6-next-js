38d34abb9a4139182aa8fae1161691645feea7dd
Swagata <swagatamondal682@gmail.com>
2026-09-24 21:25:00 +0600

    Use environment variable for API URL

--- src/app/components/WorkoutList.jsx
+++ src/app/components/WorkoutList.jsx
@@ -1,13 +1,20 @@
 import WorkoutCard from "./WorkoutCard";
 
 async function getWorkouts() {
-    const res = await fetch("https://api.abcz.workers.dev/api/fitlog");
+    try {
+        const res = await fetch(
+            process.env.NEXT_PUBLIC_SERVER_BASE_URL
+        );
 
-    if (!res.ok) {
-        throw new Error("Failed to fetch workouts");
+        if (!res.ok) {
+            throw new Error("Failed to fetch workouts");
+        }
+
+        return await res.json();
+    } catch (error) {
+        console.error("Error fetching workouts:", error);
+        return [];
     }
-
-    return res.json();
 }
 
 const WorkoutList = async () => {
